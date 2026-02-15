3f0dba6ea386b5ff2a030e684f22c46b44cc52e3
amr yakout <amrnewstory@gmail.com>
2026-02-15 10:42:14 +0300

    fine

--- DodoSweets/Program.cs
+++ DodoSweets/Program.cs
@@ -9,5 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+var supportedCultures = new[] { "en-US", "ar-SA" };
+//var localizationOptions = new RequestLocalizationOptions()
+//    .SetDefaultCulture(supportedCultures[0])
+//    .AddSupportedCultures(supportedCultures)
+//    .AddSupportedUICultures(supportedCultures);
 await builder.Build().RunAsync();
