dd704127430a460d7f00750af15a3f023a279f7d
amr yakout <amrnewstory@gmail.com>
2026-04-02 10:32:20 +0300

    add governrate

---
+++ DodoSweets/Models/Governorate.cs
@@ -0,0 +1,9 @@
+﻿namespace DodoSweets.Models
+{
+    public class Governorate
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
---
+++ DodoSweets/Models/SweetsData.cs
@@ -0,0 +1,10 @@
+﻿namespace DodoSweets.Models
+{
+    public class SweetsData
+    {
+        public List<Cake> Cakes { get; set; }
+
+        public List<Governorate> Governorates { get; set; }
+
+    }
+}
